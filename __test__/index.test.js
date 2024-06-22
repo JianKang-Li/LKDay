0dd1cef7dbcb0c41ff0fc5c78afa52271390467f
李捡康 <3104653373@qq.com>
2024-06-22 22:13:41 +0800

    删除包lock文件

---
+++ __test__/index.test.js
@@ -0,0 +1,16 @@
+import Lday from "../src/LDay"
+import { expect, test } from '@jest/globals'
+
+
+console.log(Lday)
+// test('isLeap', () => {
+//   const map = {
+//     "2004-10-11": true
+//   }
+
+//   for (const key in map) {
+//     const day = Lday(key)
+
+//     expect(day.isLeap()).toBe(map[key])
+//   }
+// })
